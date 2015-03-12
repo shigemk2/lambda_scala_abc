acdf086b1e27e994047c4db219658ba171178fe3
shigemk2 <i.am.shige@gmail.com>
2015-03-12 21:28:06 +0900

    問8 演算子 解答

---
+++ operator_without_lambda.scala
@@ -0,0 +1,11 @@
+def f1(g: (Int) => Int) = g(1)
+def f2(g: (Int, Int) => Int) = g(2, 3)
+
+// 新しい関数を定義してはいけない
+// def sub1(x:Int) = x - 3
+// def sub2(x:Int) = 3 - x
+// def add(x:Int, y: Int) = x + y
+
+println(f1(3-_))
+println(f1(_-(3)))
+println(f2(_+_))
