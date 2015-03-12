8cf285c3d1aa7dc25dbb4d1f8e8ad7034d1dd9b0
shigemk2 <i.am.shige@gmail.com>
2015-03-13 00:08:08 +0900

    flip

--- flip.scala
+++ flip.scala
@@ -1,7 +1,11 @@
 def flip[A, B, C](f: A => B => C)(x: B)(y: A) = f(y)(x)
 def append: String => String => String = a => a + _
+def sub: Int => Int => Int = a => a - _
 
 val flipped = flip(append)(_)
 println(flipped("foo")("bar"))
 println(flip(append)("foo")("bar"))
+
+println(flip(sub)(5)(3))
+println(flip(sub)(3)(5))
 
