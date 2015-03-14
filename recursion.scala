c11b6d50edbd348937693cf5277c073e38336c57
shigemk2 <i.am.shige@gmail.com>
2015-03-15 00:56:16 +0900

    問9 foldLeft再実装

--- recursion.scala
+++ recursion.scala
@@ -15,6 +15,15 @@ def mfilter(g: Int => Boolean, xs: Seq[Int]): Seq[Int] = {
 
 def mflip[A, B, C](f: (A, B) => C)(x: B)(y: A) = f(y, x)
 
+def mfoldLeft(g: (Int, Int) => Int, z: Int, xs: Seq[Int]): Int = {
+  (g, z, xs) match {
+    case (g, z, xs) if xs.isEmpty => z
+    case (g, z, xs) => mfoldLeft(g, g(z, xs.head), xs.tail)
+  }
+}
+
 println(mmap((_*2), (1 to 5)))
 println(mfilter((x => x < 5), (1 to 9)))
 println(mflip(mmap)(1 to 5)(_*2))
+println(mfoldLeft((_+_), 0, (1 to 100)))
+println(mfoldLeft((_+_), 0, (1 to 5)))
