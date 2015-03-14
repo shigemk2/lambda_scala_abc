687eba0dd01e5febb104fd8a39f98334df03b3e8
shigemk2 <i.am.shige@gmail.com>
2015-03-14 23:56:21 +0900

    問9 filter再帰再実装

--- recursion.scala
+++ recursion.scala
@@ -5,5 +5,13 @@ def mmap(g: Int => Int, xs: Seq[Int]): Seq[Int] = {
   }
 }
 
-println(mmap((x => x * 2), (1 to 5)))
+def mfilter(g: Int => Boolean, xs: Seq[Int]): Seq[Int] = {
+  xs match {
+    case xs if xs.isEmpty => Seq()
+    case xs if g(xs.head) == true  => xs.head +: mfilter(g, xs.tail)
+    case xs if g(xs.head) == false => mfilter(g, xs.tail)
+  }
+}
 
+println(mmap((x => x * 2), (1 to 5)))
+println(mfilter((x => x < 5), (1 to 9)))
