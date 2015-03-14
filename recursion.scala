a9ef2d4f0d750becf69f21f739253af37318dc66
shigemk2 <i.am.shige@gmail.com>
2015-03-15 00:58:46 +0900

    問9 foldRight再実装

--- recursion.scala
+++ recursion.scala
@@ -22,8 +22,16 @@ def mfoldLeft(g: (Int, Int) => Int, z: Int, xs: Seq[Int]): Int = {
   }
 }
 
+def mfoldRight(g: (Int, Int) => Int, z: Int, xs: Seq[Int]): Int = {
+  (g, z, xs) match {
+    case (g, z, xs) if xs.isEmpty => z
+    case (g, z, xs) => g(xs.head, mfoldRight(g, z, xs.tail))
+  }
+}
+
 println(mmap((_*2), (1 to 5)))
 println(mfilter((x => x < 5), (1 to 9)))
 println(mflip(mmap)(1 to 5)(_*2))
 println(mfoldLeft((_+_), 0, (1 to 100)))
-println(mfoldLeft((_+_), 0, (1 to 5)))
+println(mfoldLeft((_-_), 0, (1 to 5)))
+println(mfoldRight((_-_), 0, (1 to 5)))
