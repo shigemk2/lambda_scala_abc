a4ce3e93b25358d03c03a2737390118de7a6ae47
shigemk2 <i.am.shige@gmail.com>
2015-03-15 17:22:27 +0900

    filter再実装 別解

--- recursion_override.scala
+++ recursion_override.scala
@@ -1,4 +1,4 @@
-implicit def convert(xs:Seq[Int]): {def mmap(g: Int => Int): Seq[Int]} = new{
+implicit def convert1(xs:Seq[Int]): {def mmap(g: Int => Int): Seq[Int]} = new{
   def mmap(g: Int => Int): Seq[Int] = {
     xs match {
       case m if m.isEmpty => Seq()
@@ -7,4 +7,15 @@ implicit def convert(xs:Seq[Int]): {def mmap(g: Int => Int): Seq[Int]} = new{
   }
 }
 
+implicit def convert2(xs:Seq[Int]): {def mfilter(g: Int => Boolean): Seq[Int]} = new{
+  def mfilter(g: Int => Boolean): Seq[Int] = {
+    xs match {
+      case xs if xs.isEmpty => Seq()
+      case xs if g(xs.head) == true  => xs.head +: (xs.tail).mfilter(g)
+      case xs if g(xs.head) == false => (xs.tail).mfilter(g)
+    }
+  }
+}
+
 println((1 to 5).mmap((_*2)))
+println((1 to 9).mfilter((x => x < 5)))
