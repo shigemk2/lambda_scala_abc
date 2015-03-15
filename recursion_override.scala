e654021de05587f0c55e602b49da64928f25479e
shigemk2 <i.am.shige@gmail.com>
2015-03-15 18:22:04 +0900

    foldRight 再実装 別解

--- recursion_override.scala
+++ recursion_override.scala
@@ -26,7 +26,17 @@ implicit def convert3(xs:Seq[Int]): {def mfoldLeft(z: Int, g: (Int, Int) => Int)
   }
 }
 
+implicit def convert4(xs:Seq[Int]): {def mfoldRight(z: Int, g: (Int, Int) => Int): Int} = new {
+  def mfoldRight(z: Int, g: (Int, Int) => Int): Int = {
+    (z, g, xs) match {
+      case (z, g, xs) if xs.isEmpty => z
+      case (z, g, xs) => g(xs.head, (xs.tail).mfoldRight(z, g))
+    }
+  }
+}
+
 println((1 to 5).mmap((_*2)))
 println((1 to 9).mfilter((x => x < 5)))
 println((1 to 100).mfoldLeft(0, (_+_)))
-
+println((1 to 5).mfoldLeft(0, (_-_)))
+println((1 to 5).mfoldRight(0, (_-_)))
