46581f42943f470506ac6dc877f07db5ae31535b
shigemk2 <i.am.shige@gmail.com>
2015-03-15 21:55:15 +0900

    問11 クイックソート 解答

---
+++ qsort_filter.scala
@@ -0,0 +1,11 @@
+def qsort(xs: List[Int]): List[Int] = {
+  if (xs.length <= 1) xs
+  else {
+    val pivot = xs(xs.length / 2)
+    qsort(xs.filter(pivot >)) ++
+    xs.filter(pivot ==) ++
+    qsort(xs.filter(pivot <))
+  }
+}
+
+println(qsort(List(4, 6, 9, 8, 3, 5, 1, 7, 2)))
