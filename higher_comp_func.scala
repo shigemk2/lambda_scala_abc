a1ec24f9e4cbed612d6d2f5c041e35a2436c0453
shigemk2 <i.am.shige@gmail.com>
2015-03-18 00:14:12 +0900

    andThenとcomposeで高階関数の関数合成

--- higher_comp_func.scala
+++ higher_comp_func.scala
@@ -1,7 +1,10 @@
 def f(x: Int): Int = x + 1
 def g(x: Int): Int = x * 2
-def h(f: (Int) => Int) = f(1)
+def h(f: (Int) => Int): Int = f(1)
 
 println(f(g(1)))
 println(f(h(g)))
 println(h((x: Int) => f(g(x))))
+println((h _).andThen(f)(g))
+println((f _).compose(h)(g))
+
