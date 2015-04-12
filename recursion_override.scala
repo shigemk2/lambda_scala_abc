015a642d5b2150a7a37c5e349aff869ee55fbd6b
shigemk2 <i.am.shige@gmail.com>
2015-04-12 20:55:33 +0900

    再実装 言語拡張

--- recursion_override.scala
+++ recursion_override.scala
@@ -1,13 +1,20 @@
-implicit def convert1(xs:Seq[Int]): {def mmap(g: Int => Int): Seq[Int]} = new{
+// This can be achieved by adding the import clause 'import scala.language.reflectiveCalls' or by setting the compiler option -language:reflectiveCalls.
+import scala.language.implicitConversions
+import scala.language.reflectiveCalls
+
+implicit def convert1(xs:Seq[Int]): {
+  def mmap(g: Int => Int): Seq[Int]
+  def mfilter(g: Int => Boolean): Seq[Int]
+  def mfoldLeft(z: Int, g: (Int, Int) => Int): Int
+  def mfoldRight(z: Int, g: (Int, Int) => Int): Int
+} = new {
   def mmap(g: Int => Int): Seq[Int] = {
     xs match {
       case m if m.isEmpty => Seq()
       case xs: Seq[Int] => g(xs.head) +: xs.tail.mmap(g)
     }
   }
-}
 
-implicit def convert2(xs:Seq[Int]): {def mfilter(g: Int => Boolean): Seq[Int]} = new{
   def mfilter(g: Int => Boolean): Seq[Int] = {
     xs match {
       case xs if xs.isEmpty => Seq()
@@ -15,18 +22,14 @@ implicit def convert2(xs:Seq[Int]): {def mfilter(g: Int => Boolean): Seq[Int]} =
       case xs if g(xs.head) == false => (xs.tail).mfilter(g)
     }
   }
-}
 
-implicit def convert3(xs:Seq[Int]): {def mfoldLeft(z: Int, g: (Int, Int) => Int): Int} = new {
   def mfoldLeft(z: Int, g: (Int, Int) => Int): Int = {
     (z, g, xs) match {
       case (z, g, xs) if xs.isEmpty => z
       case (z, g, xs) => (xs.tail).mfoldLeft(g(z, xs.head), g)
     }
   }
-}
 
-implicit def convert4(xs:Seq[Int]): {def mfoldRight(z: Int, g: (Int, Int) => Int): Int} = new {
   def mfoldRight(z: Int, g: (Int, Int) => Int): Int = {
     (z, g, xs) match {
       case (z, g, xs) if xs.isEmpty => z
