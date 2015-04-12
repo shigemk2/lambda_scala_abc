f8eb1f7ba3230838258c61e79dbaea5f19bffcee
shigemk2 <i.am.shige@gmail.com>
2015-04-12 19:51:59 +0900

    Fix curry化

--- curry.scala
+++ curry.scala
@@ -1,6 +1,6 @@
-def add1(x: Int, y: Int) = x + y
-def add2(x: Int): (Int) => Int = (y: Int) => x + y
-def add3 = (x:Int) => (y: Int) => x + y
-println(add1(2,3))
+def add1(x: Int)(y: Int): Int = x + y
+def add2(x: Int): Int => Int = (y: Int) => x + y
+def add3 = (x: Int) => (y: Int) => x + y
+println(add1(2)(3))
 println(add2(2)(3))
 println(add3(2)(3))
