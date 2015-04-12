4a784ea3b511014b201a11a4ee076cf6b4d8d353
shigemk2 <i.am.shige@gmail.com>
2015-04-12 18:54:48 +0900

    Fix階乗

--- factorial_lambda.scala
+++ factorial_lambda.scala
@@ -1,9 +1,12 @@
-val fact : Int => Int = n => {
-  n match {
-    case 0 => 1
-    case n if n > 0 => n * fact(n - 1)
-  }
+val fact: Int => Int = n => n match {
+  case 0 => 1
+  case n if n > 0 => n * fact(n - 1)
 }
 
+// factの時点で型を決めておかないといけないからこの書き方はダメ
+// val fact = (n => n match {
+//   case 0 => 1
+//   case n if n > 0 => n * fact(n - 1)
+// }): Int => Int
 println(fact(5))
 println(fact(0))
