ab4156f0e88db87d79fc10c10a5930b439afb109
shigemk2 <i.am.shige@gmail.com>
2015-04-12 20:27:25 +0900

    Fix section

--- section.scala
+++ section.scala
@@ -1,7 +1,6 @@
-def f(g: (Int) => Int) = g(5)
-def subtract(x: Int, y: Int) = y - x
+def f(g: Int => Int) = g(5)
 
 println(List(f((x: Int) => x + 2), f(2+_)))
 println(List(f((x: Int) => x + 2), f(_+2)))
 println(List(f((x: Int) => 2 - x), f(2-_)))
-println(List(f((x: Int) => x - 2), f(_+(-2)), f(subtract(2,_))))
+println(List(f((x: Int) => x - 2), f(_-2)))
