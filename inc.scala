a94452cd15391b182d0e721608685622f8d68272
shigemk2 <i.am.shige@gmail.com>
2015-04-12 18:38:16 +0900

    Add return type

--- inc.scala
+++ inc.scala
@@ -1,3 +1,3 @@
-def inc(x: Int) = x + 1
+def inc(x: Int): Int = x + 1
 
 println(inc(5))
--- inc_lambda.scala
+++ inc_lambda.scala
@@ -1,3 +1,4 @@
-val inc = (x: Int) => x + 1
+// val inc = (x: Int) => x + 1
+val inc = (x => x + 1): Int => Int
 
 println(inc(5))
