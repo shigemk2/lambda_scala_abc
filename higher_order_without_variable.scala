47a3dcc0dbe3e598a24998c941e57e8131f876b8
shigemk2 <i.am.shige@gmail.com>
2015-04-12 19:39:22 +0900

    Fix 高階関数 ラムダ

--- higher_order_without_variable.scala
+++ higher_order_without_variable.scala
@@ -2,3 +2,9 @@ def f(g: (Int, Int) => Int) = g(2, 3)
 
 println(f((x: Int, y: Int) => x + y))
 println(f((x: Int, y: Int) => x * y))
+// 型推論されるので型は指定しなくても良い
+println(f((x, y) => x + y))
+println(f((x, y) => x * y))
+// 引数と戻り値両方の型を指定する場合
+println(f(((x, y) => x + y): (Int, Int) => Int))
+println(f(((x, y) => x * y): (Int, Int) => Int))
