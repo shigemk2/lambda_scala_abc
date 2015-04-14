d60b52bb8df875b5dc363a15eba50d83ed4de154
shigemk2 <i.am.shige@gmail.com>
2015-04-15 00:27:35 +0900

    Fix 高階関数 引数

--- higher_order_with_variable.scala
+++ higher_order_with_variable.scala
@@ -1,7 +1,7 @@
 def f(g: (Int, Int) => Int) = g(2, 3)
 
-def add(x: Int, y: Int) = x + y
-val mul = (x: Int, y: Int) => x * y
+def add(x: Int, y: Int): Int = x + y
+val mul = ((x: Int, y: Int) => x * y): (Int, Int) => Int
 
 // メソッドでも変数でもどちらでも渡せる
 println(f(add))
