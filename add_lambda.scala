7ac02306a05b70b0d7fd0614aeb6c1ef7345f4fa
shigemk2 <i.am.shige@gmail.com>
2015-04-14 23:16:04 +0900

    Fix add

--- add_lambda.scala
+++ add_lambda.scala
@@ -1,4 +1,5 @@
-val add: (Int, Int) => Int = (x, y) => x + y
+// val add: (Int, Int) => Int = (x, y) => x + y
+val add = ((x: Int, y: Int) => x + y): (Int, Int) => Int
 
 println(add(2, 3))
 println(add(9, 3))
