ba6aa6555ab6bd3e1427c980bdb817fcd13b93b8
shigemk2 <i.am.shige@gmail.com>
2015-04-14 23:54:05 +0900

    Fix mul

--- mul.scala
+++ mul.scala
@@ -1,5 +1,5 @@
-def mul1 (x: Int, y: Int) = x * y
-val mul2 = (x: Int, y: Int) => x * y
+def mul1 (x: Int, y: Int): Int = x * y
+val mul2 = ((x: Int, y: Int) => x * y): (Int, Int) => Int
 
 println(mul1(2, 3))
 println(mul2(2, 3))
