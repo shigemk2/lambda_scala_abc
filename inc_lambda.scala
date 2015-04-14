55c545e835c7623e1975efa4e05dfd0bb4711770
shigemk2 <i.am.shige@gmail.com>
2015-04-14 23:39:31 +0900

    Fix inc_lambda

--- inc_lambda.scala
+++ inc_lambda.scala
@@ -1,11 +1,3 @@
-val inc = (x: Int) => x + 1
-// val inc = (x: Int) => {
-//   x match {
-//     case 0 => 1
-//     case x => x * inc(x-1)
-//   }
-// }
 val inc = (x => x + 1): Int => Int
-val inc: Int => Int = x => x + 1
 
 println(inc(5))
